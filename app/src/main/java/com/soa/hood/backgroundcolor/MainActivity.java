db94157e4bb34415475c5b9f48235ed5091e4a7a
Inihood1 <inihood@gmail.com>
2019-05-23 17:01:20 +0100

    Instagram-background-color-animation

---
+++ app/src/main/java/com/soa/hood/backgroundcolor/MainActivity.java
@@ -0,0 +1,39 @@
+package com.soa.hood.backgroundcolor;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout layoutcontainer;
+    private AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layoutcontainer = findViewById(R.id.container);
+        animationDrawable = (AnimationDrawable) layoutcontainer.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()){
+            animationDrawable.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()){
+            animationDrawable.stop();
+        }
+    }
+}
